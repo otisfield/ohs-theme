0d872e74e9f98137cde78f5ba0b9d8a1972f402e
derekdorr <derek.dorr@gmail.com>
2016-01-20 17:23:22 -0500

    File Additions: #1 - header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+    <!-- Begin Scripts -->
+    <script src="js/lib/npo.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="js/lib/handlebars.runtime.min.js"></script>
+    <script src="js/lib/imagesloaded.pkgd.min.js"></script>
+    <script src="js/lib/jquery.masonry.min.js"></script>
+    <script src="js/templates.min.js"></script>
+    <script src="js/dev/main.js"></script>
+    <script src="js/dev/media.js"></script>
+    <!-- End Scripts -->
+
+    <?php
+        wp_footer();
+    ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#">
+<head lang="en">
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OHS</title>
+
+    <!-- Begin Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet">
+    <!-- End Styles -->
+
+    <?php
+        wp_head();
+    ?>
+
+</head>
+<body <?php body_class(); ?> data-source="data/page.json">
