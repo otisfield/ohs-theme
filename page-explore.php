32002760674c35536b258e6b7e9c7c16325bc663
derekdorr <derek.dorr@gmail.com>
2016-01-25 20:40:56 -0500

    Layout Updates

--- page-explore.php
+++ page-explore.php
@@ -1,12 +1,19 @@
 <?php
   get_header();
   global $post;
+  if (!isset($_GET['mpaged']) || $_GET['mpaged'] == '') {
+	$_GET['mpaged'] = 1;
+  }
+  $thePage = filter_var($_GET['mpaged'],FILTER_SANITIZE_NUMBER_INT);
 ?>
 	<header class="header" id="header" data-template="header" data-source="/wp-json/ohs/v1/menu"></header>
     <section class="contentTop" id="contentTop" data-template="contentTop" data-source="/wp-json/wp/v2/pages/<?=$post->ID?>"></section>
-    <div class="content" id="content" data-media="/wp-json/wp/v2/media?parent=0&per_page=30">
+    <div class="content" id="content" data-media="/wp-json/wp/v2/media?parent=0&per_page=20&page=<?=$thePage?>">
 		<aside class="cta col-md-4 grid-item piece grid-sizer" data-template="cta" data-source="/wp-json/wp/v2/ctas/448"></aside>
 		<aside class="cta col-md-8 grid-item piece grid-item--width2" data-template="cta" data-source="/wp-json/wp/v2/ctas/447"></aside>
+	</div>
+	<div class="piece pagination">
+	<a href="/explore?mpaged=<?=($thePage + 1)?>">Go to page: <?=($thePage + 1)?></a>
 	</div>
     <footer class="footer" id="footer" data-template="footer" data-source="/wp-json/ohs/v1/options"></footer>
 <?php
--- single-event.php
+++ single-event.php
@@ -4,13 +4,12 @@
 ?>
 	<header class="header" id="header" data-template="header" data-source="/wp-json/ohs/v1/menu"></header>
     <section class="contentTop" id="contentTop" data-template="contentTop" data-source="/wp-json/wp/v2/events/<?=$post->ID?>"></section>
-    <div class="content" id="content" data-mediamore="/explore" data-media="/wp-json/wp/v2/media?parent=<?=$post->ID?>">
+    <div class="content" id="content" data-mediamore="/explore" data-media="/wp-json/wp/v2/media?parent=<?=$post->ID?>&per_page=25">
 		<aside class="cta col-md-4 grid-item piece" data-template="cta" data-source="/wp-json/wp/v2/ctas/448"></aside>
         <aside class="contentTitle col-md-8 grid-item grid-item--width2 piece" data-template="contentTitle" data-source="/wp-json/wp/v2/events/<?=$post->ID?>"></aside>
 		<section class="contentBody col-md-8 grid-item grid-item--width2 piece" data-template="contentBody" data-source="/wp-json/wp/v2/events/<?=$post->ID?>"></section>
         <section class="events col-md-4 grid-item grid-sizer piece" data-template="events" data-source="/wp-json/wp/v2/events?posts_per_page=5"></section>
 		<aside class="cta col-md-8 grid-item grid-item--width2 piece" data-template="cta" data-source="/wp-json/wp/v2/ctas/447"></aside>
-		<aside class="updates col-md-8 grid-item grid-item--width2 piece" data-template="updates" data-source="/wp-json/wp/v2/updates?posts_per_page=1"></aside>
 	</div>
     <footer class="footer" id="footer" data-template="footer" data-source="/wp-json/ohs/v1/options"></footer>
 <?php
